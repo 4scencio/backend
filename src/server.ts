3309457643ee0a70220baabb4fc4797e03f12f9c
4scencio <alessandroascencio@protonmail.com>
2021-01-27 13:30:45 -0800

    routes

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,23 @@ const PORT = 3333
 
 // Method HTTP -> GET | POST | PUT | DELETE
 
+// Routes
+
+app.get('/users', (request, response) => {
+    return response.json(['User 1', 'User 2'])
+})
+
+app.post('/user', (request, response) => {
+
+})
+
+app.put('/user/:id', (request, response) => {
+
+})
+
+app.delete('/user/:id', (request, response) => {
+
+})
 
 //Server 
 app.listen(PORT, () => {
